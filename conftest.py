c3198cb78855e833cc884060bd9a9c7785566d4b
ulikww <ulikwww@ya.ru>
2016-03-30 22:37:09 +0300

    Доработан вспомогательный класс, реализующий операции login и logout

--- conftest.py
+++ conftest.py
@@ -9,18 +9,18 @@ def app(request):
     global fixture
     if fixture is None:
         fixture = Application()
-        fixture.session.Login(username="admin",password="secret")
     else:
         if not fixture.is_valid():
             fixture = Application()
             fixture.session.Login(username="admin",password="secret")
+    fixture.session.ensure_Login(username="admin",password="secret")
     return fixture
 
 
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
     def fin():
-        fixture.session.Logout()
+        fixture.session.ensure_Logout()
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
--- fixture/session.py
+++ fixture/session.py
@@ -16,6 +16,32 @@ class SessionHelper:
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
+    def ensure_Login(self,username,password):
+        wd = self.app.wd
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.Logout()
+        self.Login(username,password)
+
+
+
     def Logout(self):
         wd = self.app.wd
         wd.find_element_by_link_text("Logout").click()
+
+    def ensure_Logout(self):
+        wd = self.app.wd
+        if self.is_logged_in():
+            self.Logout()
+
+
+    def is_logged_in(self):
+        wd = self.app.wd
+        return len(wd.find_elements_by_link_text("Logout")) > 0
+
+
+    def is_logged_in_as(self,username):
+        wd = self.app.wd
+        return wd.find_element_by_xpath("//div/div[1]/form/b").text=="("+username+")"
