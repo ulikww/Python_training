ae4bd759e12170e3f757926a1e18d3b6ca76aab5
ulikww <ulikwww@ya.ru>
2016-04-01 15:34:30 +0300

    оптимизированы переходы между страницами

--- fixture/group.py
+++ fixture/group.py
@@ -40,7 +40,8 @@ class GroupHelper:
 
         def Open_group_page(self):
             wd = self.app.wd
-            wd.find_element_by_link_text("groups").click()
+            if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
+                wd.find_element_by_link_text("groups").click()
 
 
         def modification(self, group):
