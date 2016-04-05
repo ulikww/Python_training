5e3bfbad687b41490609873a43cc4a804986251d
ulikww <ulikwww@ya.ru>
2016-04-05 22:21:53 +0300

    тест модификации групп не работает
    не могу запустить проверку модификации родителя

--- model/group.py
+++ model/group.py
@@ -1,3 +1,5 @@
+from sys import maxsize
+
 class Group:
     def __init__(self,name=None,header=None,footer=None,Parent_group=None,id=None):
         self.name = name
@@ -8,9 +10,15 @@ class Group:
 
 
     def __repr__(self):
-        return "%s:%s" % (self.id,self.name)
+        return "%s:%s:%s" % (self.id,self.name,self.Parent_group)
 
     def __eq__(self, other):
-        return (self.id is None or other.id is None or self.id == other.id) and self.name == other.name
+        return (self.id is None or other.id is None or self.id == other.id) and self.name == other.name and self.Parent_group == other.Parent_group
 
+
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize
 
--- test/test_modification_group.py
+++ test/test_modification_group.py
@@ -1,10 +1,15 @@
 from model.group import Group
 
 def test_modification(app):
-    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="dgdgdgd", header="dgdgdgd", footer="dgdgdg"))
         app.group.create(Group(name="родитель", header="родитель", footer="родитель"))
-    app.group.modification(Group(Parent_group="родитель"))
+    old_groups = app.group.get_group_list()
+    group = Group(Parent_group="родитель")
+    group.id = old_groups[0].id
+    app.group.modification(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key = Group.id_or_max)==sorted(new_groups, key = Group.id_or_max)
+
