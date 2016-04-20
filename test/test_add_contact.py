7adfbd1430cc4066a069067c9c8d6e6acee6a479
ulikww <ulikwww@ya.ru>
2016-04-20 23:05:01 +0300

    Отделение тестовых данных для контактов
    Отделение тестовых данных от сценариев (DDT продолжается) для контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,23 +1,7 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
 import pytest
-import random
-import string
-
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range (random.randrange(maxlen))])
-
-
-testdata = [Contact(firstname="", middlename="", lastname="", nickname="", company="", address="",email="")] + [
-        Contact(firstname="Ульяна", middlename="Владимировна", lastname="Ватракшина", nickname="ulik", company="1c",
-        address="дмитровское ш 9",email="ulikwwwww@ya.ru", homephone="3434343", mobilephone="4454545", workphone="657676",
-        secondaryphone="88888")] +[Contact(firstname=random_string("firstname",23),middlename=random_string("middlename",24),
-                                           lastname=random_string("lastname",8))]
-
-
-
+from data.add_contact import testdata
 
 @pytest.mark.parametrize ("contact", testdata, ids=[repr(x) for x in testdata])
 def test_add_contact(app, contact):
