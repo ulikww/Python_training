d50879693eea1de7e3a12d784b42632295ff20f9
ulikww <ulikwww@ya.ru>
2016-04-27 19:58:37 +0300

    тест по удалению контакта из группы. в процессе

---
+++ test/test_del_contact_from_group.py
@@ -0,0 +1,25 @@
+from model.contact import Contact
+from fixture.orm import ORMFixture
+import random
+
+
+db = ORMFixture(host = '127.0.0.1',name = 'addressbook', user = 'root', password = '')
+
+
+def test_del_contact_from_group(app, db, json_groups):
+    old_contacts = db.get_contact_list()
+    old_groups = db.get_group_list()
+    if len(old_groups) == 0:
+        group = json_groups
+        app.group.create(group)
+        if len(old_contacts) == 0:
+            app.contact.create(
+               Contact(firstname="Ульяна", middlename="Владимировна", lastname="Ватракшина", nickname="ulik", company="1c",
+                        address="дмитровское ш 9",email="ulikwwwww@ya.ru"))
+        #contact = random.choice(old_contacts)
+        #app.contact.delete_contact_by_id(contact.id)
+        #new_contacts = db.get_contact_list()
+        #old_contacts.append(contact)
+        #assert sorted(old_contacts,key = Contact.id_or_max) == sorted(new_contacts,key = Contact.id_or_max)
+        #if check_ui:
+     #   assert sorted(new_contacts, key = Contact.id_or_max) == sorted(app.contact.get_contact_list(), key = Contact.id_or_max )
